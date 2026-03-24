0e7554d6ad7f4ad15581d08f844335ebcc16b2ab
cyberxdevsofficial <cyberxdevsofficial@gmail.com>
2026-03-24 14:43:40 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,24 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVumXqnV4AFzP9uk275v9-7yrMlt4kGe4",
+  authDomain: "anugahostingzz.firebaseapp.com",
+  projectId: "anugahostingzz",
+  storageBucket: "anugahostingzz.firebasestorage.app",
+  messagingSenderId: "182948977720",
+  appId: "1:182948977720:web:044de7bcbd76645b5c1acd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Auth & Providers
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const githubProvider = new GithubAuthProvider();
+
+// Firestore
+export const db = getFirestore(app);
